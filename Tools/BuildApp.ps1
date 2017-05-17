9d41014838c30ddd7974b65a4bf235efcb446b87
Keiho Sakapon <git@saka-pon.net>
2017-05-17 18:56:57 +0900

    Add build tools

---
+++ Tools/BuildApp.ps1
@@ -0,0 +1,13 @@
+﻿# $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+
+.\IncrementVersion-cs.ps1 ..\WlcAgent\WlcAgentConsole
+
+$slnFilePath = "..\WlcAgent\WlcAgent.sln"
+& $msbuild $slnFilePath /p:Configuration=Release /t:Clean
+& $msbuild $slnFilePath /p:Configuration=Release /t:Rebuild
+
+# In case of PowerShell, the work directory is that of .ps1 file.
+.\CreateZipForAssembly-cs.ps1 ..\WlcAgent\WlcAgentConsole\bin\Release\WlcAgentConsole.exe ..\Downloads
+
+explorer ..\Downloads
