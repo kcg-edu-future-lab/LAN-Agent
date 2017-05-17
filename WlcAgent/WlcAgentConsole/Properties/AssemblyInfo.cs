1eaf97fe764f6a604e194e86b4ad1220427be60d
Keiho Sakapon <git@saka-pon.net>
2017-05-17 15:48:13 +0900

    Update project settings

--- WlcAgent/WlcAgentConsole/Properties/AssemblyInfo.cs
+++ WlcAgent/WlcAgentConsole/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("WlcAgentConsole")]
+[assembly: AssemblyTitle("WLC Agent Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WlcAgentConsole")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("KTools")]
+[assembly: AssemblyCopyright("© 2017 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
