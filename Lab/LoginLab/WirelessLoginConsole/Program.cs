c46484658f04b06478f5af04efd55c5d2a30032e
Keiho Sakapon <git@saka-pon.net>
2017-07-25 11:17:07 +0900

    Log in to WLC

--- Lab/LoginLab/WirelessLoginConsole/Program.cs
+++ Lab/LoginLab/WirelessLoginConsole/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WirelessLoginConsole
 {
     class Program
     {
+        static string LoginUri { get; } = ConfigurationManager.AppSettings["LoginUri"];
+        static string Username { get; } = ConfigurationManager.AppSettings["Username"];
+        static string Password { get; } = ConfigurationManager.AppSettings["Password"];
+
         static void Main(string[] args)
         {
+            Login(Username, Password).Wait();
+        }
+
+        public static async Task Login(string username, string password)
+        {
+            var data = new Dictionary<string, string>
+            {
+                { "buttonClicked", "4" },
+                { "err_flag", "0" },
+                { "err_msg", "" },
+                { "info_flag", "0" },
+                { "info_msg", "" },
+                { "redirect_url", "" },
+                { "username", username },
+                { "password", password },
+            };
+            var content = new FormUrlEncodedContent(data);
+
+            using (var http = new HttpClient())
+            {
+                await http.PostAsync(LoginUri, content);
+            }
         }
     }
 }
