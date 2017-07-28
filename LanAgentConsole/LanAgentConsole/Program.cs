e6c319ba58fba86e7c503e7d1cbf094920e9c506
Keiho Sakapon <git@saka-pon.net>
2017-07-28 16:01:43 +0900

    Log in to WLC

--- LanAgentConsole/LanAgentConsole/Program.cs
+++ LanAgentConsole/LanAgentConsole/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace LanAgentConsole
@@ -14,6 +14,33 @@ namespace LanAgentConsole
 
         static void Main(string[] args)
         {
+            try
+            {
+                Main2().Wait();
+            }
+            catch (AggregateException ex) when (ex.InnerException is HttpRequestException)
+            {
+                Console.WriteLine("Network Error");
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        static async Task Main2()
+        {
+            var accessor = new WlcAccessor
+            {
+                LoginUri = "https://wlc.int.kcg.edu/login.html",
+                HeartbeatUri = "http://www.kcg.ac.jp/images/index/center_line_bg.gif",
+            };
+
+            var isLoggedIn = await accessor.IsLoggedInAsync();
+            if (isLoggedIn) return;
+
+            var result = await accessor.LoginAsync(Username, Password);
+            Console.WriteLine(result);
         }
     }
 }
