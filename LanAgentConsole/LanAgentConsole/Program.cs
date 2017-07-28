0d6a0f86ccc1e1e5ce2150619cd19e0ddace5eab
Keiho Sakapon <git@saka-pon.net>
2017-07-28 15:28:00 +0900

    Add app settings

--- LanAgentConsole/LanAgentConsole/Program.cs
+++ LanAgentConsole/LanAgentConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace LanAgentConsole
 {
     class Program
     {
+        static string Username { get; } = ConfigurationManager.AppSettings["Username"];
+        static string Password { get; } = ConfigurationManager.AppSettings["Password"];
+
         static void Main(string[] args)
         {
         }
