e983b65f71b3c5b8f8b95f5193fdee4153af92d5
Keiho Sakapon <git@saka-pon.net>
2017-07-28 15:30:31 +0900

    Add classes for LAN access

---
+++ LanAgentConsole/LanAgentConsole/LanAccessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LanAgentConsole
+{
+    public abstract class LanAccessor
+    {
+        public string LoginUri { get; set; }
+        public string HeartbeatUri { get; set; }
+
+        /// <summary>
+        /// Check whether the user is already logged in to the LAN.
+        /// </summary>
+        /// <returns>A value that indicates whether the user is already logged in to the LAN.</returns>
+        public abstract Task<bool> IsLoggedInAsync();
+
+        /// <summary>
+        /// Log in to the LAN.
+        /// </summary>
+        /// <param name="username">A username.</param>
+        /// <param name="password">A password.</param>
+        /// <returns>The result of the login.</returns>
+        public abstract Task<LoginResult> LoginAsync(string username, string password);
+    }
+
+    public enum LoginResult
+    {
+        LoginSucceeded,
+        LoginFailed,
+        AlreadyLoggedIn,
+    }
+}
